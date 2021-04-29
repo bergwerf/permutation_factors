c3e7ec70b7322136c9a6bc85071bfc61e3c78a22
Herman Bergwerf <hermanbergwerf@gmail.com>
2021-04-30 01:05:45 +0200

    Rename some things.

--- C1_Schreier_vector.v
+++ C1_Schreier_vector.v
@@ -8,7 +8,7 @@ Definition vector := fmap perm.
 
 Section Algorithm.
 
-Fixpoint step (i : positive) (π : perm)
+Fixpoint extend (i : positive) (π : perm)
   (gen : list perm) (V : vector)
   (acc : list positive) :=
   match gen with
@@ -16,38 +16,38 @@ Fixpoint step (i : positive) (π : perm)
   | σ :: gen' =>
     let j := σ⋅i in
     match lookup V j with
-    | Some _ => step i π gen' V acc
-    | None => step i π gen' (insert V j (σ ∘ π)) (j :: acc)
+    | Some _ => extend i π gen' V acc
+    | None => extend i π gen' (insert V j (σ ∘ π)) (j :: acc)
     end
   end.
 
 Variable generator : list perm.
 
-Fixpoint cycle (V : vector) (try acc : list positive) :=
+Fixpoint step (V : vector) (try acc : list positive) :=
   match try with
   | [] => (V, acc)
   | i :: try' =>
     match lookup V i with
     | Some π =>
-      match step i π generator V acc with
-      | (V', acc') => cycle V' try' acc'
+      match extend i π generator V acc with
+      | (V', acc') => step V' try' acc'
       end
-    | None => cycle V try' acc
+    | None => step V try' acc
     end
   end.
 
-Fixpoint iterate (V : vector) (try : list positive) (n : nat) :=
+Fixpoint loop (V : vector) (try : list positive) (n : nat) :=
   match n with
   | O => V
   | S m =>
-    match cycle V try [] with
+    match step V try [] with
     | (V', []) => V'
-    | (V', try') => iterate V' try' m
+    | (V', try') => loop V' try' m
     end
   end.
 
-Definition build (size k : positive) :=
-  iterate (insert Leaf k ident) [k] (Pos.to_nat size).
+Definition build (range k : positive) :=
+  loop (insert Leaf k ident) [k] (Pos.to_nat range).
 
 End Algorithm.
 
