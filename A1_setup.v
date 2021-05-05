b38d95bcfe3a2a1af9c175caf983cec79a98c619
Herman Bergwerf <hermanbergwerf@gmail.com>
2021-05-06 01:54:32 +0200

    Add Module SGChain.

--- A1_setup.v
+++ A1_setup.v
@@ -14,10 +14,6 @@ Notation "'Σ' x .. y , P" := (sigT (λ x, .. (sigT (λ y, P)) ..))
   (at level 200, x binder, y binder, right associativity,
   format "'[ ' '[ ' Σ  x .. y ']' ,  '/' P ']'") : type_scope.
 
-(* Functions on nat without importing the proofs *)
-Notation min := (Nat.min).
-Notation max := (Nat.max).
-
 (* Cartesian products. *)
 Notation "A × B" := (prod A B) (at level 100).
 
--- C3_generator_chain.v
+++ C3_generator_chain.v
@@ -1,1 +1,31 @@
 (* The Schreier-Sims algorithm to factor a group into a chain of subgroups. *)
+
+From CGT Require Import A1_setup B1_finite_map B2_permutation.
+From CGT Require Import C1_Schreier_vector C2_Sims_filter.
+
+(* Factor a group into a sub-group chain. *)
+Module SGChain.
+
+Definition sieve4 := identity_sieve 4 xH.
+
+(* list of triples (A, k, V) where orbit V = orbit of k in ⟨A⟩). *)
+Definition chain := list (list perm × positive × Schreier.vector).
+
+Fixpoint loop sieve range gen ks :=
+  if (length gen =? 0)%nat then []
+  else match ks with
+  | [] => []
+  | k :: ks' =>
+    let V := Schreier.build gen k range in
+    let gen' := Schreier.generator gen k V in
+    let gen'' := Sims.filter sieve range gen' in
+    (gen, k, V) :: loop sieve range gen'' ks'
+  end.
+
+Definition build gen ks :=
+  let range := fold_left Pos.max ks 1 in
+  let width := Pos.size_nat range in
+  let sieve := identity_sieve width xH in
+  loop sieve (Pos.to_nat range) gen ks.
+
+End SGChain.
